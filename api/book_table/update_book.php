3c11538573b75fd14a16842f55449b3384ccd33f
Smit Chitroda <143987366+IamSmitChitroda@users.noreply.github.com>
2024-11-26 19:30:43 +0530

    Update update_book.php`

--- api/book_table/update_book.php
+++ api/book_table/update_book.php
@@ -7,13 +7,13 @@ $conn_res = $congin->connect();
 
 header('Content-Type: application/json');
 
-$book = $_POST['book'];
+$name = $_POST['book'];
 $id = $_POST['id'];
 $arr = array();
 
 if ($id != null && $name != null) {
     if ($conn_res) {
-        $sql = "UPDATE temp SET `book` = '$book' WHERE `id`= $id";
+        $sql = "UPDATE `book_table` SET `book` = '$name' WHERE `id`= $id";
         $response = mysqli_query($conn_res, $sql);
         if ($response) {
             $arr = array("status_code" => "200", "message" => "Data Update Successfully", "is_success" => "true");
@@ -30,7 +30,7 @@ if ($id != null && $name != null) {
         http_response_code(500);
     }
 } else {
-    $arr = array("status_code" => "400", "message" => "Id and Book are must be required", "is_success" => "false");
+    $arr = array("status_code" => "400", "message" => "Id and Name are must be required", "is_success" => "false");
     echo json_encode($arr, JSON_PRETTY_PRINT);
     http_response_code(400);
 }
